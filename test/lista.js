42da076cf859d4d7b88d557c15cc53d3ac176519
Pablo <pabloarancibia.dw@gmail.com>
2022-03-16 19:27:09 -0300

    clase 2

---
+++ test/lista.js
@@ -0,0 +1,7 @@
+const expect = require("chai").expect;
+
+describe ("lista de pares clave:valor", function(){
+    it("En una lista vacia hay cero elementos", function(){
+        expect(1).to.equal(2);
+    })
+})
